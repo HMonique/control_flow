c90d2f71c37711e5d460da4ee286d7ca039c85a9
HMonique <161358207+HMonique@users.noreply.github.com>
2024-05-20 22:41:23 -0400

    control_flow
    git commit -m control_flow

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("Testing Control Flow");
+
+// This is my bathroom
+/**
+ * Scoping 
+ * Block: a section of code that separated pieces of code in its own little environment 
+ * Fuction: 
+ * Globla:
+ * Module:
+ * 
+ * 
+ */
+//{
+ //   const washHands = "Sink";
+ //   const lookAtYourself = "Mirror";
+//}
+
+// Create a function 
+
+//function addition(a, b, c) {
+//    console.log(a + b + c);
+//}
+
+//addition(1, 3, 5);
+
+function calculator(num1, num2, operator) {
+    if (operator === "+") {
+        console.log(num1 + num2);
+    }
+    if (operator === "-") {
+        console.log(num1 - num2);
+    }
+}
+
+// take two numbers and also an operator
